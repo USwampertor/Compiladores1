1ae2d8ae12305290d244ee193061be7e1b0946a7
Marco <idv16c.mmillan@uartesdigitales.edu.mx>
2018-05-17 17:42:25 -0500

    +added project

---
+++ Compilers_UI/Compilers_UI/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compilers_UI
+{
+    public partial class Form1 : Form
+    {
+        CompilersCore.Manager compiler = new CompilersCore.Manager();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCore();
+        }
+        public void InitializeCore()
+        {
+        }
+        private void debugToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void compilarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String[] compDetails = compiler.compile("");
+            compileOutput.Text = compDetails[0];
+        }
+    }
+}
